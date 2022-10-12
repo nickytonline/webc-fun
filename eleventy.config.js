4441818393af4ba92e75afeb4f0f339a2a5d6d67
Nick Taylor <nick@iamdeveloper.com>
2022-10-12 15:13:19 -0400

    chore: formatted some files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,5 @@
 module.exports = (config) => {
-  config.addPlugin(require('@11ty/eleventy-plugin-webc'), {
-    components: "_includes/components/*.webc",
-  });
-
-}
+	config.addPlugin(require('@11ty/eleventy-plugin-webc'), {
+		components: '_includes/components/*.webc',
+	});
+};
